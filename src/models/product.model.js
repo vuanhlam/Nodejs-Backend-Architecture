9cc295592874a6654115a6b1f04976e307d6d037
Vu Anh Lam <vuanhlam000@gmail.com>
2023-12-16 22:46:16 +0700

    Schema Product Ecommerce

---
+++ src/models/product.model.js
@@ -0,0 +1,77 @@
+const { Schema, model } = require("mongoose"); // Erase if already required
+
+const DOCUMENT_NAME = "Product";
+const COLLECTION_NAME = "Products";
+
+var productSchema = new Schema(
+  {
+    product_name: {
+      type: String,
+      required: true,
+    },
+    product_thumb: {
+      type: String,
+      required: true,
+    },
+    product_description: String,
+    product_price: {
+      type: Number,
+      required: true,
+    },
+    product_quantity: {
+      type: Number,
+      required: true,
+    },
+    product_type: {
+      type: String,
+      required: true,
+      enum: ["Electronics", "Clothing", "Furniture"],
+    },
+    product_shop: {
+      type: Schema.Types.ObjectId,
+      ref: "Shop",
+    },
+    product_attributes: {
+      type: Schema.Types.Mixed,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  }
+);
+
+// define the product type  = clothing
+
+const clothingSchema = new Schema(
+  {
+    brand: { type: String, required: true },
+    size: String,
+    material: String,
+  },
+  {
+    collection: "Clothes",
+    timeseries: true,
+  }
+);
+
+// define the product type  = electronic
+const electronicSchema = new Schema(
+  {
+    manufacturer: { type: String, required: true },
+    model: String,
+    color: String,
+  },
+  {
+    collection: "Electronics",
+    timeseries: true,
+  }
+);
+
+//Export the model
+module.exports = {
+  product: model(DOCUMENT_NAME, productSchema),
+  electronic: model("Electronic", electronicSchema),
+  clothing: model("Clothes", clothingSchema),
+};
