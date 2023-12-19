e952b2345ecd9ce85efb164dfcb3d4d15de18dc6
Vu Anh Lam <vuanhlam000@gmail.com>
2023-12-19 14:21:15 +0700

    fix product model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,40 +1,30 @@
 const { Schema, model } = require("mongoose"); // Erase if already required
+const slugfify = require('slugify')
 
 const DOCUMENT_NAME = "Product";
 const COLLECTION_NAME = "Products";
 
 var productSchema = new Schema(
   {
-    product_name: {
-      type: String,
-      required: true,
-    },
-    product_thumb: {
-      type: String,
-      required: true,
-    },
+    product_name: { type: String, required: true},
+    product_thumb: { type: String, required: true},
     product_description: String,
-    product_price: {
-      type: Number,
-      required: true,
+    product_slug: String,
+    product_price: { type: Number, required: true},
+    product_quantity: { type: Number, required: true},
+    product_type: { type: String, required: true, enum: ["Electronics", "Clothing", "Furniture"]},
+    product_shop: { type: Schema.Types.ObjectId, ref: "Shop"},
+    product_attributes: { type: Schema.Types.Mixed, required: true},
+    product_ratingAverage: { 
+      type: Number, 
+      default: 4.5, 
+      min: [1, 'Rating must be above 1.0'], 
+      max: [5, 'Rating must be belove 5.0'], 
+      set: (val) => Math.round(val * 10) / 10
     },
-    product_quantity: {
-      type: Number,
-      required: true,
-    },
-    product_type: {
-      type: String,
-      required: true,
-      enum: ["Electronics", "Clothing", "Furniture"],
-    },
-    product_shop: {
-      type: Schema.Types.ObjectId,
-      ref: "Shop",
-    },
-    product_attributes: {
-      type: Schema.Types.Mixed,
-      required: true,
-    },
+    product_variations: { type: Array, default: []},
+    isDraft: { type: Boolean, default: true, index: true, select: false},
+    isPublished: { type: Boolean, default: false, index: true, select: false}
   },
   {
     timestamps: true,
@@ -92,6 +82,12 @@ const furnitureSchema = new Schema(
     timeseries: true,
   }
 );
+
+// Document Middleware run before .save() and .create()
+productSchema.pre('save', function(next) {
+  this.product_slug = slugfify(this.product_name, { lower: true })
+  next();
+})
 
 //Export the model
 module.exports = {
