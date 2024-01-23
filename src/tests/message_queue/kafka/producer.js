daf894a0e8901488342c28bbd946120169f6107f
Vu Anh Lam <vuanhlam000@gmail.com>
2024-01-23 21:58:15 +0700

    test kafka

---
+++ src/tests/message_queue/kafka/producer.js
@@ -0,0 +1,20 @@
+const { Kafka } = require("kafkajs");
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+const producer = kafka.producer();
+
+const runProducer = async () => {
+  await producer.connect();
+  await producer.send({
+    topic: "test-topic",
+    messages: [{ value: "Hello KafkaJS user by Vu Anh Lam!" }],
+  });
+
+  await producer.disconnect();
+};
+
+runProducer().catch(console.error);
